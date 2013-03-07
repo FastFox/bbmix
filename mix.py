1b76ae3ce3e63fadae031305ca2da3bb1f2434c0
Michiel <git@fastfox.nl>
2013-03-07 19:01:08 +0100

    Load bioharness csv file

--- mix.py
+++ mix.py
@@ -1,1 +1,21 @@
-print 'hi'
+import csv
+
+class Mix():
+	bioharness = None
+
+	def __init__(self):
+		print 'Mix init'
+		self.bioharness = csv.reader(open('2012_04_23-02_52_33_General.csv', 'r'))
+		i = 0
+		for row in self.bioharness:
+			if i == 0:
+				print row
+			i += 1
+			if i == 60:
+				i = 0
+
+def main():
+	M = Mix()
+
+if __name__ == "__main__":
+	main()
